21e644c44173807079cdb5a0a18a90da14eaeaec
Barry Roberts <barryjroberts@gmail.com>
2025-02-11 11:08:56 +0900

    not sure...

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_id, only: [:show, :edit, :update, :destroy]
+
+  def set_id
+    @task = Task.find(params[:id])
+  end
 
   def index
-  @tasks = Task.all
+    @tasks = Task.all
   end
 
   def show
-  @task = Task.find(params[:id])
   end
 
   def new
@@ -21,29 +25,25 @@ class TasksController < ApplicationController
     end
   end
 
-    def edit
-      @task = Task.find(params[:id])
-    end
+  def edit
+  end
 
-    def update
-      @task = Task.find(params[:id])
-      if @task.update(task_params)
-        redirect_to task_path(@task)
-      else
-        render :edit, status: :unprocessable_entity
-      end
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-      @task = Task.find(params[:id])
-      @task.destroy
-      redirect_to tasks_path, status: :see_other
-    end
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
-
 end
